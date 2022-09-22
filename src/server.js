8f358cb44f1764178036e7c528b8faa3b7d2ecbc
Jordan Yamada <sadaotomokane@gmail.com>
2022-09-22 06:07:16 +0000

    added proof of life

--- src/server.js
+++ src/server.js
@@ -6,6 +6,15 @@ const logger = require('./middleware/logger.js');
 const validator = require('./middleware/validator.js');
 
 app.use(logger);
+
+app.get('/', (request, response) => {
+  try {
+    response.status(200).send('Proof of life');
+  } catch(e) {
+    console.log(e);
+  }
+});
+
 app.get('/person', validator, (request, response) => {
   response.json({
     name: request.query.name,
